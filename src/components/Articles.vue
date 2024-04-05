fb37cade93a649cf976e2fff5be036e04ef77c6c
korotsheOt <butenko.dev@gmail.com>
2024-04-05 12:18:39 +0300

    cleanup & add Articles component

---
+++ src/components/Articles.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import "../data";
+
+const articlesData = ref(window.LATEST_ARTICLES);
+
+</script>
+
+<template>
+	<div class="wrapper">
+		<h2>Latest Updates</h2>
+		<div class="articles-list">
+			<input type="checkbox" id="news" value="news" />
+			<label for="news">News</label>
+			<input type="checkbox" id="essay" value="essay" />
+			<label for="essay">Essay</label>
+
+			<ul>
+				<li v-for="article in articlesData" :key="article.title">
+					<span>{{ article.title }}</span>
+					<span>{{ article.publishDate }}</span>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+</style>
