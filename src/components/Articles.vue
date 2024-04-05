c8d6bffc3a09da352b9a9c1be23acf5aedbf2bcd
korotsheOt <butenko.dev@gmail.com>
2024-04-05 12:39:35 +0300

    add filtering by category

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -3,20 +3,45 @@ import { ref } from "vue";
 import "../data";
 
 const articlesData = ref(window.LATEST_ARTICLES);
+const filteredArticlesData = ref();
+const checkedCategories = ref([]);
 
+const handleCategoryCheck = () => {
+	if (checkedCategories.value.length) {
+		filteredArticlesData.value = articlesData.value.filter((article) => {
+			return checkedCategories.value.includes(article.category)
+		}
+		);
+	} else {
+		filteredArticlesData.value = [];
+	}
+
+	filteredArticlesData.value = filteredArticlesData.value.slice(0, 5);
+};
 </script>
 
 <template>
 	<div class="wrapper">
 		<h2>Latest Updates</h2>
 		<div class="articles-list">
-			<input type="checkbox" id="news" value="news" />
+			<input 
+				type="checkbox" 
+				id="news" 
+				value="news" 
+				v-model="checkedCategories" 
+				@change="handleCategoryCheck()" />
 			<label for="news">News</label>
-			<input type="checkbox" id="essay" value="essay" />
+
+			<input 
+				type="checkbox" 
+				id="essay" 
+				value="essay" 
+				v-model="checkedCategories"
+				@change="handleCategoryCheck()" />
 			<label for="essay">Essay</label>
 
 			<ul>
-				<li v-for="article in articlesData" :key="article.title">
+				<li v-for="article in filteredArticlesData" :key="article.title">
 					<span>{{ article.title }}</span>
 					<span>{{ article.publishDate }}</span>
 				</li>
